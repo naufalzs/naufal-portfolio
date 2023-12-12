ec2829bddc4267970328a9fdc8d9194fe28d855a
Naufal Zufar S <naufal.zufar.s@gmail.com>
2023-12-12 17:41:51 +0700

    feat: add app scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { About, Header, Footer, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 
+import "./App.scss";
+
 export default function App() {
   return (
     <div className="app">
