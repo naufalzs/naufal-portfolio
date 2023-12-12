54ff116594136db4dcf7989c715e905f76cdfa1b
Naufal Zufar S <naufal.zufar.s@gmail.com>
2023-12-12 14:51:59 +0700

    feat: setup folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { About, Header, Footer, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 }
